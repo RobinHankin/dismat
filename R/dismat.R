77ac4ff3c6968a75666d5b05d530236c1c57ae10
Robin Hankin <hankin.robin@gmail.com>
2023-05-02 08:26:19 +1200

    initial upload

---
+++ R/dismat.R
@@ -0,0 +1,101 @@
+setClass("dismat",contains = "VIRTUAL", slots = c(x="matrix",hash="character"))
+setMethod("elements","dismat",function(x){x@x})  # no occurences of "@" below this line
+setMethod("dim","dismat",function(x){dim(elements(x))})
+
+setClass("dismat_rd_ci",
+         slots    = c(x="matrix",rowhash="character"),
+         contains = "dismat"
+         )
+
+setClass("dismat_ri_cd",
+         slots    = c(x="matrix",colhash="character"),
+         contains = "dismat"
+         )
+
+setClass("dismat_rd_cd",
+         slots    = c(x="matrix",rowhash="character",colhash="character"),
+         contains = "dismat"
+         )
+
+`dismat_rd_ci` <- function(M,hash=NULL){
+  if(is.null(hash)){h <- hashcal(M)}
+  new("dismat_ri_cd",x=M,hash=h)
+}
+
+`dismat_ri_cd` <- function(M){new("dismat_ri_cd",x=M,hash=hashcal(M))}
+
+setMethod("show", "dismat_rd_ci", function(object){disord_rd_ci_show(object)})
+
+`disord_rd_ci_show` <- function(x){
+    cat("A dismat_rd_ci object with hash",hash(x), "and elements\n")
+    jj <- x@x
+    rownames(jj) <- rep("-",nrow(jj))
+    print(jj)
+    cat("\n(the rows are in some implementation-specific order)\n")
+    return(invisible(x))
+}
+
+setMethod("[", signature(x="dismat_rd_ci",i="disindex",j="ANY",drop="ANY"),
+          function(x,i,j,drop=TRUE){
+	      stopifnot(identical(hash(x),hash(i)))
+              if(missing(j)){
+                  out <- elements(x)[.value(i),,drop=drop]
+              } else {
+                  out <- elements(x)[.value(i), j, drop=drop]
+              }
+              out <- dismat_rd_ci(out)
+              if(drop){
+                  return(drop(out))
+              } else {
+                  return(out)
+              }
+          } )
+
+
+setMethod("[", signature(x="dismat_rd_ci",i="disord",j="ANY",drop="ANY"),
+          function(x,i,j,drop=TRUE){
+	  stopifnot(is.logical(i)) # NB:  m <- matrix(1:35,5,7); compare m[c(1,0,0,1,0),] with m[c(T,F,F,T,F),]
+	  if(missing(j)){
+	    out <- x[which(i), ,drop=drop]  # which(i) is a disindex object
+	  } else {
+  	    out <- x[which(i),j,drop=drop]
+	  }
+	  return(out)
+	  } )
+
+
+setMethod("[", signature(x="dismat_rd_ci",i="ANY",j="ANY",drop="ANY"),
+          function(x,i,j,drop=TRUE){
+              stop("\nfor objects of class 'dismat_rd_ci', the [non-missing, non-empty] row accessor
+	      must be of either class 'disindex' or Boolean of class 'disord'; and column accessor
+	      must be of class index")
+          } )
+
+setGeneric("rowSums")
+setMethod("rowSums",signature(x="dismat_rd_ci"),function(x, na.rm = FALSE, dims = 1L){
+stop("not yet written")
+} )
+
+setMethod("apply",signature(X="dismat_rd_ci"),
+          function (X, MARGIN, FUN, i_am_sure=FALSE, ..., simplify = TRUE){
+              out <- apply(elements(X),MARGIN,FUN,...,simplify=simplify)
+              if(MARGIN == 1){
+                  return(disord(out,h=hash(X)))
+                  } else {
+                      if(!i_am_sure){
+                          stop("\nArgument 'i_am_sure' is FALSE by default.
+			  If you are _sure_ that FUN is disord-invariant, call apply() with argument i_am_sure=TRUE")
+                          } else {
+                              return(out)
+                          }
+                  }
+          } )
+
+
+
+
+
+setClass("dismat_symm",
+         slots    = c(x="matrix",hash="character"),
+         contains = "dismat"
+         )
